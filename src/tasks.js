13bc3a57dac76335941fc026af36674793a85934
Lucas <lucas_river_2814@hotmail.com>
2023-03-22 12:43:37 -0300

    first commit

---
+++ src/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi Primer Tarea",
+    description: "Mi Primer Terea",
+  },
+  {
+    id: 1,
+    title: "Mi Segunda Tarea",
+    description: "Mi Segunda Terea",
+  },
+  {
+    id: 2,
+    title: "Mi Tercer Tarea",
+    description: "Mi Tercer Terea",
+  },
+  {
+    id: 3,
+    title: "Mi Cuarta Tarea",
+    description: "Mi Cuarta Terea",
+  },
+];
