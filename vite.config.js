28c76bb6ca37fdc2f25184e1eb1718310b21d2b5
Lucas <lucas_river_2814@hotmail.com>
2023-03-29 07:32:05 -0300

    tercer Commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tasks-react/'
+  base: 'https://Lucas281402.github.io/tasks-react'
 })
